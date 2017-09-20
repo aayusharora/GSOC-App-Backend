a04fb5d35d32e40b49773ca32c50c9f44fb431ef
aayusharora <aayush113002@gmail.com>
2017-09-20 22:19:48 +0530

    fixed log

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const bp = require('body-parser');
 const api_v1 = require('./api_v1');
 
 var app = express();
-
-var PORT =  process.env.port || 5000 ;
-
 app.get('/', function(req,res) {
 
     res.send('Hello World');
@@ -16,7 +13,7 @@ app.use(bp.json());
 app.use(bp.urlencoded({extended : true}));
 app.use('/api_v1',api_v1);
 
-app.listen( PORT, function() {
+app.listen( process.env.PORT || 5000 , function() {
     console.log("Serving running on Port " + PORT);
 });
 
